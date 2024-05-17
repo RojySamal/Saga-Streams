a91319b24cb70d329262de154e70a2d847aa308a
Chandan <hyeiamcosmos@gmail.com>
2024-05-17 15:37:25 +0530

    Chip Rendering

--- Frontend/src/Components/Post.jsx
+++ Frontend/src/Components/Post.jsx
@@ -15,6 +15,8 @@ import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Chip } from "@mui/material";
+import PropTypes from "prop-types";
+
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
@@ -26,29 +28,25 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-export default function Post({...props}) {
+export default function Post({blogTitle, blogTopic, blogSummary, blogImage, blogContent}) {
 
-   console.log('Blogtitle: ',props.blogTitle);
-  console.log('Topics: ',props.blogTopic);
-  const topics = props.blogTopic
+
   const [expanded, setExpanded] = React.useState(false);
-  // const [title,setTitle] = useState("");
-  // const [content,setContent] = useState("");
-  // const [summary, setSummary] = useState("");
-  // const [imageURL,setImageURl] = useState("");
-  // const [topics,setTopics] = useState([]);
-
-  // React.useEffect(()=>{
-
-  //   setTitle(blog.title)
-  //   setContent(blog.content)
-  //   setSummary(blog.summary)
-  //   setImageURl(blog.imageUrl)
-  //   setTopics(blog.topic)
-  // },[])
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
+  };
+
+  Post.propTypes = {
+    blogTitle: PropTypes.string.isRequired,
+    blogTopic: PropTypes.arrayOf(PropTypes.string),
+    blogSummary: PropTypes.string.isRequired,
+    blogImage: PropTypes.string,
+    blogContent: PropTypes.string.isRequired,
+  };
+
+  Post.defaultProps = {
+    blogTopic: [],
   };
 
   return (
@@ -64,24 +62,24 @@ export default function Post({...props}) {
             <MoreVertIcon />
           </IconButton>
         }
-        title={props.blogTitle}
+        title={blogTitle}
         subheader="September 14, 2016"
       />
       <CardMedia
         component="img"
         height="194"
-        image={props.blogImage}
-        alt="Paella dish"
+        image={blogImage}
+        alt={blogTitle}
       />
       <CardContent>
-      {/* <div>
+      { <div>
       
-      {(topics).map((topic, index) => (
+      {(blogTopic).map((topic, index) => (
         <Chip key={index} label={topic} style={{ margin: '0.5rem' }} />
       ))}
-      </div> */}
+      </div> }
         <Typography variant="body2" color="text.secondary">
-          {props.blogSummary}
+          {blogSummary}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -103,35 +101,8 @@ export default function Post({...props}) {
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
         <Typography>
-          {props.blogContent}
+          {blogContent}
         </Typography>
-          {/* <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
-          </Typography> */}
         </CardContent>
       </Collapse>
     </Card>
